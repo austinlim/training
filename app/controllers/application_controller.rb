74d490b8f1c488af8844616b6926a96644844a9d
cclim <cclim@cclim.(none)>
2013-06-20 22:43:19 +0800

    Modify application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,8 @@ class ApplicationController < ActionController::Base
   before_filter :authenticate_user!
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  #testing push to server
+
   protected
 
   def configure_permitted_parameters
