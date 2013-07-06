eea6f7cd12ec526d7d564f3a53decf54b73f5cff
cclim <austincclim@gmail.com>
2013-07-06 17:06:41 +0800

    Testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   #testing push to server
+  #testing
 
   protected
 
